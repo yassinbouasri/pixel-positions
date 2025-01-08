a012a7b1be6efde351471e9db0c55723ba916f30
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-01-08 23:05:43 +0100

    Initial commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b  border-white/10">
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Pixel Positions">
+                </a>
+            </div>
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+            <div>
+                <a href="#">Post a job</a>
+            </div>
+        </nav>
+
+        <main class="py-10">
+            {{ $slot }}
+        </main>
+    </div>
+
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'black': "#060606"
+            }
+        },
+    },
+    plugins: [],
+};
