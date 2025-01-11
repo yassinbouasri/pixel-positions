145a0c134c687347c62c15145c65f3fffcfc1204
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-01-11 23:54:26 +0100

    Add TagController and tag-job relationship
    Introduce a TagController to handle tag-specific job listings and a `jobs` method in the Tag model to define a many-to-many relationship with jobs. Updated routes to support tag-based job filtering and disabled lazy loading prevention in the AppServiceProvider for flexibility during development.

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use App\Models\Tag;
+
+class TagController extends Controller
+{
+    public function __invoke(Tag $tag)
+    {
+        return view('results', ['jobs' => $tag->jobs]);
+    }
+}
